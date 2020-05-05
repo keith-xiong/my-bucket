d031d188407c7ef67897b964d9e39efb742c45bd
keith <qx@qianxi.ml>
2020-05-06 02:15:48 +0800

    1. remove hub installation for it has already been there 2. add hosts to connect directly to the utools resource server to get    latest updates

--- bin/hacks.ps1
+++ bin/hacks.ps1
@@ -1,3 +1,4 @@
+# 添加 timestamp 替换变量支持
 $coreps = "$(scoop prefix scoop)\lib\core.ps1"
 
 if ((Get-Content $coreps -Tail 1) -eq "setup_proxy") {
@@ -5,9 +6,15 @@ if ((Get-Content $coreps -Tail 1) -eq "setup_proxy") {
 
 $substituteOrig = $function:substitute
 function substitute($entity, [Hashtable] $params, [Bool]$regexEscape = $false) {
-    $params['$timestamp'] = Get-Date | % Subtract ([datetime]"1970.1") | % Ticks
+    $params['$timestamp'] = Get-Date -UFormat %s
     & $substituteOrig $entity $params $regexEscape
 }
 '@ |
 Out-File $coreps -Append -Encoding ascii
 }
+
+# 设置 hosts 直连获取更新
+"`n`n124.161.37.35 resource.u-tools.cn" |
+Out-File C:\Windows\System32\drivers\etc\hosts -Append -Encoding ascii
+
+ipconfig /flushdns
